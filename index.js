8465bac01794dd2653b374524a59e302e8ab4178
LucasufbaCC <lucassouza@dcc.ufba.br>
2017-12-14 21:24:10 -0300

    feito ajustes no arquivo js

---
+++ index.js
@@ -0,0 +1,21 @@
+class Message{
+
+	constructor(){
+		this.text = '';
+		this.created = Date.now();
+	}
+
+}
+
+var messageVazia = new Message();
+
+
+
+console.log('teste4');
+console.log('teste3');
+console.log('teste2');
+console.log('teste');
+console.log(messageVazia);
+console.log('novo');
+console.log('novo2');
+console.log('novo4');
