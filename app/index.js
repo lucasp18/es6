60dc5d9f61a9b883761800c084bf5638c7e9345e
LucasufbaCC <lucassouza@dcc.ufba.br>
2018-01-08 23:59:54 -0300

    arquivos aula

--- app/index.js
+++ app/index.js
@@ -1,24 +1,22 @@
-import moment from 'moment';
-import Message from './model/message.model';
-import template from './messages.html';
-import './styles/modules/MessageBox.scss';
-import './styles/modules/MessagesArea.scss';
-import logo from './images/especializa_logo.jpg';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var moment_1 = require("moment");
+var message_model_1 = require("./model/message.model");
+var messages_html_1 = require("./messages.html");
+require("./styles/modules/MessageBox.scss");
+require("./styles/modules/MessagesArea.scss");
+var especializa_logo_jpg_1 = require("./images/especializa_logo.jpg");
 console.log('Index started');
-console.dir(new Message());
-
+console.dir(new message_model_1.default());
 /* eslint no-undef: 0 */
-document.getElementById('send').onclick = () => {
-  const m = new Message(document.getElementById('message').value);
-  document.getElementById('messages').innerHTML += template({
-    m,
-    relativeTime: moment(m.created).fromNow(),
-  });
+document.getElementById('send').onclick = function () {
+    var m = new message_model_1.default(document.getElementById('message').value);
+    document.getElementById('messages').innerHTML += messages_html_1.default({
+        m: m,
+        relativeTime: moment_1.default(m.created).fromNow(),
+    });
 };
-
-document.getElementById('logo').src = logo;
-
+document.getElementById('logo').src = especializa_logo_jpg_1.default;
 if (module && module.hot) {
-  module.hot.accept();
+    module.hot.accept();
 }
