f7b93cb3eb53f7ce70e78965fc112734fd288fef
LucasufbaCC <lucassouza@dcc.ufba.br>
2018-01-08 23:01:38 -0300

    teste da branch

---
+++ app/playground.js
@@ -0,0 +1,8 @@
+var m = "teste";
+var n = 10;
+var array = ['l', 'u', 'c'];
+var tuples = ['A', 10];
+console.log(m);
+console.log(n);
+console.log(array);
+console.log(tuples);
---
+++ app/playground.ts
@@ -0,0 +1,12 @@
+const m:string = "teste";
+const n:number = 10;
+
+const array: string[] = ['l','u','c'];
+
+const tuples: [string, number] = [ 'A' , 10];
+console.log(m);
+console.log(n);
+console.log(array);
+console.log(tuples);
+console.log('teste');
+
